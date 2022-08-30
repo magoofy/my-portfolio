1fb192f3c9258bf97d24c61c18a1142557c3bd8c
sakau <sakau@usc.edu>
2022-08-29 21:40:45 -0700

    branch

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './Components/Header';
+import About from './Components/pages/About';
+import Contact from './Components/pages/Contact';
+import Projects from './Components/pages/Projects';
+import Resume from './Components/pages/Resume';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
